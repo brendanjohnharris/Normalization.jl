42b6e1d6fcb8a67a8b126d3242b13b4cdcb647c5
brendanjohnharris <brendanjohnharris@gmail.com>
2022-04-03 00:33:30 +1100

    Delete unnecessary file

--- src/ZScore.jl
+++
@@ -1,7 +0,0 @@
-Base.@kwdef mutable struct ZScore <: AbstractNormalization
-    dims
-    p::Union{Nothing, NTuple{2, AbstractArray}} = nothing
-    𝑝::NTuple{2, Function} = (mean, std)
-    𝑓::Function = (x, 𝜇, 𝜎)->(x .- 𝜇)./𝜎
-    𝑓⁻¹::Function = (y, 𝜇, 𝜎) -> y.*𝜎 .+ 𝜇
-end
