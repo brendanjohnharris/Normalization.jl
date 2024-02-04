451248a36edebd4044be61e3f8a16a273dd97a2d
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-02-04 15:50:11 +1100

    Tweak fix

--- src/Normalization.jl
+++ src/Normalization.jl
@@ -67,7 +67,7 @@ end
 
 # * Robust versions of typical 2-parameter normalizations
 common_norms = [:ZScore, :Sigmoid,]
-function _iqr(x::AbstractVector{T})::T where {T}
+function _iqr(x::AbstractArray{T})::T where {T}
     eltype(x).((quantile(x[:], 0.75) - quantile(x[:], 0.25))/1.35) # ? Divide by 1.35 so that std(x) ≈ _iqr(x) when x contains normally distributed values
 end
 _robustNorm(N::Symbol; name="Robust"*string(N)|>Symbol) = eval(:(@_Normalization $name (median, _iqr) ($N)().𝑓 ($N)().𝑓⁻¹))
